94c61d68e87e72ff7e3c58baa3cde07764bac66a
jayden1252 <140182914+jayden1252@users.noreply.github.com>
2023-07-26 12:48:59 -0300

    Add files via upload

---
+++ desafio1.rb
@@ -0,0 +1,11 @@
+#Criação de um cadastro de biblioteca
+print "digite o seu Nome: "
+nome = gets.chomp
+
+print "digite o seu Sobrenome: "
+sobrenome = gets.chomp
+
+print "digite a sua idade"
+idade = gets.chomp.to_i
+
+puts" Meu nome é #{nome}#{sobrenome} e tenho #{idade}"
